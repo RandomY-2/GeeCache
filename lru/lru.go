fdc0a420820a7a59850d08f5504625f8498a27f0
RandomY-2 <69881587+RandomY-2@users.noreply.github.com>
2022-07-05 17:09:37 -0700

    lru cache

---
+++ lru/lru.go
@@ -0,0 +1,74 @@
+package lru
+
+import "container/list"
+
+type Cache struct {
+	maxBytes  int64
+	nbytes    int64
+	listPtr   *list.List
+	cache     map[string]*list.Element
+	OnEvicted func(key string, value Value)
+}
+
+type entry struct {
+	key   string
+	value Value
+}
+
+type Value interface {
+	Len() int
+}
+
+func New(maxBytes int64, nbytes int64, OnEvicted func(key string, value Value)) *Cache {
+	return &Cache{
+		maxBytes:  maxBytes,
+		nbytes:    nbytes,
+		listPtr:   list.New(),
+		OnEvicted: OnEvicted,
+	}
+}
+
+func (c *Cache) Get(key string) (value Value, ok bool) {
+	if element, ok := c.cache[key]; ok {
+		c.listPtr.MoveToFront(element)
+		kv := element.Value.(*entry)
+		return kv.value, true
+	}
+	return
+}
+
+// RemoveOldest removes the oldest item
+func (c *Cache) RemoveOldest() {
+	element := c.listPtr.Back()
+	if element != nil {
+		c.listPtr.Remove(element)
+		kv := element.Value.(*entry)
+		delete(c.cache, kv.key)
+		c.nbytes -= int64(len(kv.key)) + int64(kv.value.Len())
+		if c.OnEvicted != nil {
+			c.OnEvicted(kv.key, kv.value)
+		}
+	}
+}
+
+// Add adds a value to the cache.
+func (c *Cache) Add(key string, value Value) {
+	if element, ok := c.cache[key]; ok {
+		c.listPtr.MoveToFront(element)
+		kv := element.Value.(*entry)
+		c.nbytes += int64(value.Len()) - int64(kv.value.Len())
+		kv.value = value
+	} else {
+		element := c.listPtr.PushFront(&entry{key, value})
+		c.cache[key] = element
+		c.nbytes += int64(len(key)) + int64(value.Len())
+	}
+	for c.maxBytes != 0 && c.maxBytes < c.nbytes {
+		c.RemoveOldest()
+	}
+}
+
+// Len the number of cache entries
+func (c *Cache) Len() int {
+	return c.listPtr.Len()
+}
